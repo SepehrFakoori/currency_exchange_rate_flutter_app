d810dd51fb0546fc81e951ac1d30a66a54c70022
Sepehr <sepehrfakoori@gmail.com>
2024-07-06 23:05:49 +0330

    Insert App Colors

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:currency_exchange_rate_app_flutter/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,48 +7,68 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
       body: SafeArea(
-        child: Center(
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black,
-                  blurRadius: 10,
-                  spreadRadius: -2,
-                  offset: Offset(0, 2),
-                ),
-              ],
+        child: CustomScrollView(
+          slivers: [
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return const CardContainer();
+                },
+              ),
             ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "USD",
-                      style: Theme.of(context).textTheme.headlineLarge,
-                    ),
-                    Text("United State Dollar"),
-                  ],
-                ),
-                const Spacer(),
-                Text(
-                  "59,000 IRT",
-                  style: Theme.of(context).textTheme.headlineLarge,
-                ),
-              ],
-            ),
-          ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class CardContainer extends StatelessWidget {
+  const CardContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      height: 100,
+      decoration: BoxDecoration(
+        color: AppColors.backgroundColor,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 10,
+            spreadRadius: -5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "USD",
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              const Text("United State Dollar"),
+            ],
+          ),
+          const Spacer(),
+          Text(
+            "59,000 IRT",
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+        ],
       ),
     );
   }
