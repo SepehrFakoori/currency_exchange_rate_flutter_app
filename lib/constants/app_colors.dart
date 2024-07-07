79e2650a35b219ca8f120aae74d1f9e191e57d9d
Sepehr <sepehrfakoori@gmail.com>
2024-07-07 20:38:25 +0330

    Colors Changed!

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const backgroundColor = Color(0xffe5e5e5);
+  static const backgroundColor = Color(0xff8cebca);
   static Color highEmphasisColor = Colors.black.withOpacity(0.87);
   static Color mediumEmphasisColor = Colors.black.withOpacity(0.60);
   static Color lowEmphasisColor = Colors.black.withOpacity(0.38);
-  static const priceColor = Color(0xff6e62bb);
+  static const priceColor = Color(0xff0f917c);
 }
