15cf8b5332fb4a64ae3fc363fcb5922c7265667f
Sepehr <sepehrfakoori@gmail.com>
2024-07-07 20:41:31 +0330

    Update homeScreen UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,40 +1,150 @@
 import 'package:currency_exchange_rate_app_flutter/constants/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  return const CardContainer();
-                },
-                childCount: 10,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Sepehr Fakoori",
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  SvgPicture.asset("assets/images/settings.svg"),
+                ],
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Text(
+                "Exchange Rate",
+                style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                      color: AppColors.priceColor,
+                    ),
+              ),
+              const SizedBox(height: 20),
+              TabBarContainer(tabController: _tabController),
+              const SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    CustomScrollView(
+                      slivers: [
+                        SliverList(
+                          delegate: SliverChildBuilderDelegate(
+                            (context, index) {
+                              return CardContainer(index);
+                            },
+                            childCount: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                    CustomScrollView(
+                      slivers: [
+                        SliverList(
+                          delegate: SliverChildBuilderDelegate(
+                            (context, index) {
+                              return CardContainer(index);
+                            },
+                            childCount: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
+class TabBarContainer extends StatelessWidget {
+  const TabBarContainer({
+    super.key,
+    required TabController? tabController,
+  }) : _tabController = tabController;
+
+  final TabController? _tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: TabBar(
+        controller: _tabController,
+        labelColor: Colors.white,
+        labelStyle:
+            Theme.of(context).textTheme.headlineSmall!.copyWith(fontSize: 16),
+        unselectedLabelColor: AppColors.lowEmphasisColor,
+        indicator: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: AppColors.priceColor,
+        ),
+        indicatorPadding: const EdgeInsets.all(3),
+        dividerColor: Colors.transparent,
+        indicatorSize: TabBarIndicatorSize.tab,
+        tabs: const [
+          Tab(
+            text: "All",
+          ),
+          Tab(
+            text: "Favorites",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class CardContainer extends StatelessWidget {
-  const CardContainer({
+  final int index;
+
+  const CardContainer(
+    this.index, {
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      margin: const EdgeInsets.symmetric(vertical: 5),
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
       height: 130,
       decoration: BoxDecoration(
@@ -53,69 +163,88 @@ class CardContainer extends StatelessWidget {
           ),
         ],
       ),
-      child: Column(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              Expanded(
-                child: Row(
+          Text(
+            "${index + 1}",
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    const Icon(Icons.money),
-                    const SizedBox(width: 4),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          const Icon(Icons.money),
+                          const SizedBox(width: 4),
+                          Text(
+                            "United State Dollar",
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineMedium!
+                                .copyWith(
+                                  color: AppColors.highEmphasisColor,
+                                ),
+                          ),
+                        ],
+                      ),
+                    ),
                     Text(
-                      "United State Dollar",
+                      "USD",
                       style:
-                          Theme.of(context).textTheme.headlineMedium!.copyWith(
-                                color: AppColors.highEmphasisColor,
+                          Theme.of(context).textTheme.headlineSmall!.copyWith(
+                                color: AppColors.lowEmphasisColor,
                               ),
                     ),
                   ],
                 ),
-              ),
-              Text(
-                "USD",
-                style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                      color: AppColors.lowEmphasisColor,
+                const Spacer(),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "59,000",
+                      style:
+                          Theme.of(context).textTheme.headlineSmall!.copyWith(
+                                color: Colors.black.withOpacity(0.38),
+                              ),
                     ),
-              ),
-            ],
-          ),
-          const Spacer(),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "59,000",
-                style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                      color: Colors.black.withOpacity(0.38),
+                    const SizedBox(width: 5),
+                    Text(
+                      "IRT",
+                      style:
+                          Theme.of(context).textTheme.headlineSmall!.copyWith(
+                                color: AppColors.lowEmphasisColor,
+                              ),
                     ),
-              ),
-              const SizedBox(width: 5),
-              Text(
-                "IRT",
-                style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                      color: AppColors.lowEmphasisColor,
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "59,000",
+                      style:
+                          Theme.of(context).textTheme.headlineLarge!.copyWith(
+                                color: AppColors.priceColor,
+                              ),
                     ),
-              ),
-            ],
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "59,000",
-                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                      color: AppColors.priceColor,
+                    const SizedBox(width: 5),
+                    Text(
+                      "IRT",
+                      style:
+                          Theme.of(context).textTheme.headlineMedium!.copyWith(
+                                color: AppColors.priceColor,
+                              ),
                     ),
-              ),
-              const SizedBox(width: 5),
-              Text(
-                "IRT",
-                style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                      color: AppColors.priceColor,
-                    ),
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
