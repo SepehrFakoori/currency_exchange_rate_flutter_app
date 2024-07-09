2004a0ed7cfcca69941f10166d49b5968f1bf5c0
Sepehr <sepehrfakoori@gmail.com>
2024-07-09 18:43:24 +0330

    Insert di with get_it

---
+++ lib/di/di.dart
@@ -0,0 +1,10 @@
+import 'package:currency_exchange_rate_app_flutter/data/datasource/crypto_currency_datasource.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+var locator = GetIt.instance;
+
+Future<void> getItInit() async {
+  locator.registerSingleton<Dio>(Dio(BaseOptions(baseUrl: "https://one-api.ir/")));
+  locator.registerSingleton<ICryptoCurrencyDataSource>(CryptoCurrencyRemoteDatasource());
+}
